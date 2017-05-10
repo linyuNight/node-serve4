97b73545fd57368694904dfb1ea61307bb83aa03
qq526645879 <526645879@qq.com>
2017-05-10 18:03:56 +0800

    add

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = require('express')();
 var ejs = require('ejs');// 后台模板库
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mysql=require('mysql'); 
 var compression = require('compression');//开启gzip
 app.use(compression());//开启gzip
 
@@ -11,16 +12,60 @@ app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
 app.use(express.static('public/www'));
 
+
+var connection = mysql.createConnection({
+ host   : '127.0.0.1',
+ user   : 'root',
+ password : 'root',
+ database : 'chat'
+});
+connection.connect();
+// connection.query("select * from chat_content" , function selectTable(err, rows, fields){
+//  if (err){
+//   throw err;
+//  }
+//  if (rows){
+//   for(var i = 0 ; i < rows.length ; i++){
+//    console.log("%s\t%s\t%s", rows[i].zzzz,rows[i].age,rows[i].age2);
+//   }
+//  }
+// });
+// connection.query('insert into chat_content (name ,content) values ("lupeng" , "123456")');
+ 
+// connection.end();
+
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/www/index.html');
   // res.render("index",{title:'header'});
 });
 
+var chatlist = [];
+app.get('/chatdata', function(req, res){
+	res.header("Access-Control-Allow-Origin", "*");
+	connection.query("select * from chat_content" , function selectTable(err, rows, fields){
+		if (err){
+			throw err;
+		}
+		if (rows){
+			chatlist = []
+			for(var i = 0 ; i < rows.length ; i++){
+				chatlist.push(rows[i])
+			}
+		}
+	});
+	res.json({chatlist: chatlist});
+});
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+  	connection.query('insert into chat_content (name ,content) values ("'+msg[0]+'" , "'+msg[1]+'")');
     io.emit('chat message', msg);
+    // console.log(msg);
   });
 });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
